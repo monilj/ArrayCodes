a19222ba52bf648f158681e132a2e7b3adbee63b
Qualitia Software <webadmin@qualitiasoft.com>
2020-04-18 17:49:08 +0530

    Added code for duplicate element using hashtable

---
+++ src/DuplicateElementInArrayUsingHashTable.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class DuplicateElementInArrayUsingHashTable {
+
+    public static void main(String[] args) {
+        String[] names = { "Java", "JavaScript", "Python", "C", "Ruby", "Java" };
+
+        Map<String, Integer> nameAndCount = new HashMap<>();
+        // build hash table with count
+        for (String name : names) {
+            Integer count = nameAndCount.get(name);
+            if (count == null) {
+                nameAndCount.put(name, 1);
+            } else {
+                nameAndCount.put(name, ++count);
+            }
+        }
+        Set<Map.Entry<String, Integer>> entrySet = nameAndCount.entrySet();
+        for (Map.Entry<String, Integer> entry : entrySet) {
+
+            if (entry.getValue() > 1) {
+                System.out.println("Duplicate element from array : "
+                        + entry.getKey());
+            }
+        }
+
+
+    }
+
+    }
+
+
